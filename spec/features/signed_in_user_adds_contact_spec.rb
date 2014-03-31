ca8c87c73041a62e04c17584c42e40494c1712a1
Simon Taranto <simon.taranto@gmail.com>
2014-03-31 13:51:58 -0700

    initial feature test for adding contact

---
+++ spec/features/signed_in_user_adds_contact_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'User adds a contact' do
+  scenario 'with valid name and frequency' do
+    sign_in
+    visit root_path
+    fill_in 'Name', with: 'Nick'
+    fill_in 'Frequency', with: 7
+    click_on 'Add'
+    # assert the name is on page
+    expect(page).to have_content('Nick')
+    expect(page).to have_content(7)
+  end
+
+  def sign_in
+    user = create(:user)
+    visit sign_in_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Sign in'
+  end
+end
