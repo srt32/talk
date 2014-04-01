53c28496f3d73d282ea7b61d36e3614dcf3da1ac
Simon Taranto <simon.taranto@gmail.com>
2014-03-31 18:37:54 -0700

    make frequency a dropdown list

--- spec/features/signed_in_user_adds_contact_spec.rb
+++ spec/features/signed_in_user_adds_contact_spec.rb
@@ -6,20 +6,11 @@ feature 'User adds a contact' do
     sign_in
     visit root_path
     fill_in 'Name', with: 'Nick'
-    fill_in 'Frequency', with: 7
+    select('Weekly', :from => 'Frequency')
     click_on 'Add'
     expect(page).to have_content("added Nick")
     expect(page).to have_content('Nick')
     expect(page).to have_content(7)
-  end
-
-  scenario 'with invalid frequency' do
-    sign_in
-    visit root_path
-    fill_in 'Name', with: 'Nick'
-    fill_in 'Frequency', with: 0
-    click_on 'Add'
-    expect(page).to have_content('Frequency is not included')
   end
 
 end
